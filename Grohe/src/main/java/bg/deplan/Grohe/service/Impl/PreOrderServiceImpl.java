8cdf06176bd92f9ada5fc7031fb8f3448bc0886e
Plamen Georgiev <plamen_gg@mail.bg>
2025-02-21 16:32:50 +0200

    Create smart add order reason in excel import. Read comment, find template, translate from cyrillic to latin.

--- Grohe/src/main/java/bg/deplan/Grohe/service/Impl/PreOrderServiceImpl.java
+++ Grohe/src/main/java/bg/deplan/Grohe/service/Impl/PreOrderServiceImpl.java
@@ -103,7 +103,7 @@ public class PreOrderServiceImpl implements PreOrderService {
         Optional<Article> optionalArticle;
 
         String artNum = preOrderDTO.artNum();
-        if(preOrderDTO.artNum().isEmpty()) {
+        if (preOrderDTO.artNum().isEmpty()) {
             optionalArticle = articleRepository.findByArtNum(preOrderItem.getArticle().getArtNum());
         } else {
             optionalArticle = articleRepository.findByArtNum(preOrderDTO.artNum());
@@ -290,11 +290,66 @@ public class PreOrderServiceImpl implements PreOrderService {
             preOrderItem.setOrderBy("Вили");
             preOrderItem.setDate(preOrderExcelItems.date());
             //TODO: need to create method that translate cyrillic to latin
-            preOrderItem.setOrderReason("Stocks");
+            preOrderItem.setOrderReason(checkComment(preOrderExcelItems.comment()));
             preOrderItem.setComment(preOrderExcelItems.comment());
 
             preOrderItemRepository.save(preOrderItem);
         }
+    }
+
+    private String checkComment(String comment) {
+        if(comment == null) {
+            return "Stocks";
+        }
+
+        String[] splitComment = comment.split(" ");
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 1; i < splitComment.length; i++) {
+            stringBuilder.append(splitComment[i] + " ");
+        }
+
+        String commentsInfo = stringBuilder.toString();
+        String translateComment = transliterate(commentsInfo);
+
+        for (int i = 0; i < splitComment.length; i++) {
+            String currentWord = splitComment[i].toLowerCase();
+
+            if (currentWord.contains("промo")) {
+                return "Promo offer";
+            }
+
+            if (currentWord.contains("мостр")) {
+                return "Samples " + translateComment;
+            }
+
+            if (currentWord.contains("проек")) {
+                return "Project " + translateComment;
+            }
+
+            if (currentWord.contains("офер")) {
+                return "Project " + translateComment;
+            }
+            if (currentWord.contains("ofert")) {
+                return "Project " + translateComment;
+            }
+        }
+
+        return "Stocks";
+    }
+
+    public static String transliterate(String message){
+        char[] abcCyr =   {' ','а','б','в','г','д','е','ё', 'ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х', 'ц','ч', 'ш','щ','ъ','ы','ь','э', 'ю','я','А','Б','В','Г','Д','Е','Ё', 'Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х', 'Ц', 'Ч','Ш', 'Щ','Ъ','Ы','Ь','Э','Ю','Я','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9'};
+        String[] abcLat = {" ","a","b","v","g","d","e","e","zh","z","i","y","k","l","m","n","o","p","r","s","t","u","f","h","ts","ch","sh","sch", "","i", "","e","ju","ja","A","B","V","G","D","E","E","Zh","Z","I","Y","K","L","M","N","O","P","R","S","T","U","F","H","Ts","Ch","Sh","Sch", "","I", "","E","Ju","Ja","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9"};
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < message.length(); i++) {
+            for (int x = 0; x < abcCyr.length; x++ ) {
+                if (message.charAt(i) == abcCyr[x]) {
+                    builder.append(abcLat[x]);
+                }
+            }
+        }
+        return builder.toString();
     }
 
 }
