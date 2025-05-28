5ad316a6bb5f24b3666b948ff2a3c3522d691873
Plamen Georgiev <plamen_gg@mail.bg>
2025-05-28 14:30:00 +0300

    Fix search sorting

--- Grohe/src/main/java/bg/deplan/Grohe/data/OrderItemRepository.java
+++ Grohe/src/main/java/bg/deplan/Grohe/data/OrderItemRepository.java
@@ -19,13 +19,13 @@ public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
     void deleteAllByOrderId(@Param("orderId") long id);
 
 //    @Query("SELECT oi FROM OrderItem oi WHERE oi.orderBy = :orderBy")
-    @Query("SELECT oi FROM OrderItem oi WHERE oi.orderBy LIKE %:orderBy%")
+    @Query("SELECT oi FROM OrderItem oi WHERE oi.orderBy LIKE %:orderBy% ORDER BY oi.order")
     List<OrderItem> findOrderItemsByOrderBy(@Param("orderBy") String orderBy);
 
-    @Query("SELECT oi FROM OrderItem oi WHERE oi.article.id = :article")
+    @Query("SELECT oi FROM OrderItem oi WHERE oi.article.id = :article ORDER BY oi.order")
     List<OrderItem> findOnlyArticlesInOrder(@Param("article") long article);
 
-    @Query("SELECT oi FROM OrderItem oi WHERE oi.article.id IN :ids")
+    @Query("SELECT oi FROM OrderItem oi WHERE oi.article.id IN :ids ORDER BY oi.order")
     List<OrderItem> findOrderItemsByIDs(@Param("ids") List<Long> ids);
 
     @Query("SELECT oi FROM OrderItem oi WHERE oi.order.id = :id")
