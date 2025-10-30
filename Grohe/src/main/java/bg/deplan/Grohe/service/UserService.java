742e4bf23358f9609a59c7a54a44994ce2f92eb4
Plamen Georgiev <plamen_gg@mail.bg>
2025-10-30 14:52:38 +0200

    test github workflow

--- Grohe/src/main/java/bg/deplan/Grohe/service/UserService.java
+++ Grohe/src/main/java/bg/deplan/Grohe/service/UserService.java
@@ -3,5 +3,7 @@ package bg.deplan.Grohe.service;
 import bg.deplan.Grohe.model.DTOs.UserRegisterDTO;
 
 public interface UserService {
+//    User registration
     void registerUser(UserRegisterDTO userRegisterDTO);
+
 }
